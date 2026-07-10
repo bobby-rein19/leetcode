aa45cf9437507d9c777f4109b1b94e7a36bc2ece
bobby <bobbykushwah19082005@gmail.com>
2026-07-10 18:37:52 +0530

    Time: 0 ms (100%), Space: 19.3 MB (65.17%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {')': '(', ']': '[', '}': '{'}
+        
+        for char in s:
+            if char in pairs:  # closing bracket
+                if not stack or stack[-1] != pairs[char]:
+                    return False
+                stack.pop()
+            else:  # opening bracket
+                stack.append(char)
+        
+        return not stack
