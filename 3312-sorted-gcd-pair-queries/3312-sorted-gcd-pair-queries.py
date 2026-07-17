533ed73dc67dec45431a6bffb85e0a2f5c4eba42
bobby <bobbykushwah19082005@gmail.com>
2026-07-17 19:55:51 +0530

    Time: 787 ms (22.5%), Space: 39.6 MB (72.5%) - LeetHub

--- 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.py
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.py
@@ -1,16 +1,38 @@
 class Solution:
-    def gcdValues(self, nums: List[int], queries: List[int]) -> List[int]:
-        M = max(nums)
-        cnt = [0] * (M + 1)
-        for x in nums: cnt[x] += 1
-        ex = [0] * (M + 1)
-        for d in range(1, M + 1):
-            c = sum(cnt[d::d])
-            ex[d] = c * (c - 1) // 2
-        for d in range(M, 0, -1):
-            for m in range(2 * d, M + 1, d):
-                ex[d] -= ex[m]
-        for d in range(1, M + 1):
-            ex[d] += ex[d - 1]
-        from bisect import bisect_right
-        return [bisect_right(ex, q) for q in queries]
+     def gcdValues(self, nums: List[int], queries: List[int]) -> List[int]:
+        maxV = max(nums)
+        cnt = [0] * (maxV + 1)
+        for x in nums:
+            cnt[x] += 1
+
+        cntMultiple = [0] * (maxV + 1)
+        for d in range(1, maxV + 1):
+            total = 0
+            for m in range(d, maxV + 1, d):
+                total += cnt[m]
+            cntMultiple[d] = total
+
+        exact = [0] * (maxV + 1)
+        for d in range(1, maxV + 1):
+            c = cntMultiple[d]
+            exact[d] = c * (c - 1) // 2
+
+        for d in range(maxV, 0, -1):
+            for m in range(2 * d, maxV + 1, d):
+                exact[d] -= exact[m]
+
+        prefix = [0] * (maxV + 1)
+        for d in range(1, maxV + 1):
+            prefix[d] = prefix[d - 1] + exact[d]
+
+        ans = []
+        for q in queries:
+            lo, hi = 1, maxV
+            while lo < hi:
+                mid = (lo + hi) // 2
+                if prefix[mid] > q:
+                    hi = mid
+                else:
+                    lo = mid + 1
+            ans.append(lo)
+        return ans
